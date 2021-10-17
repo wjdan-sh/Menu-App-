b56a72abb2a1d7a804dad58654368bd8c58ef199
wjdan <walsharekh3@gmail.com>
2021-10-17 16:00:22 +0300

    Menu-App-

---
+++ app/src/main/java/com/example/menuapp/About.kt
@@ -0,0 +1,39 @@
+package com.example.menuapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class About : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.home -> {
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.help -> {
+                val intent = Intent(this, Help::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.about -> {
+                val intent = Intent(this, About::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
